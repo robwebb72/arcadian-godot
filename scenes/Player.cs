5501000de00b25b3efc1d752ef2c1b839a61fb94
Rob Webb <robstah72@gmail.com>
2025-07-21 11:18:54 +0100

    Add collision detection to player

--- scenes/Player.cs
+++ scenes/Player.cs
@@ -5,7 +5,12 @@ public partial class Player : Area2D
 {
 	private Vector2 _screenSize;
 	[Export] public int Speed = 300;
+	
 	private Vector2 _velocity;
+	private CollisionPolygon2D _defaultCollisionPoly;
+	private CollisionPolygon2D _leftCollisionPoly;
+	private CollisionPolygon2D _rightCollisionPoly;
+
 
 	private AnimatedSprite2D _sprite;
 
@@ -15,6 +20,10 @@ public partial class Player : Area2D
 		_sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 		_sprite.Play();
 		_sprite.Animation = "default";
+		_defaultCollisionPoly = GetNode<CollisionPolygon2D>("DefaultCollisionPoly");
+		_leftCollisionPoly = GetNode<CollisionPolygon2D>("LeftCollisionPoly");
+		_rightCollisionPoly = GetNode<CollisionPolygon2D>("RightCollisionPoly");
+		AreaEntered += OnAreaEntered;	// had to wite this in by hand... seems strange
 	}
 
 	public void Initialise(Vector2 screenSize)
@@ -38,18 +47,45 @@ public partial class Player : Area2D
 
 
 		Position = new Vector2(
-			Math.Clamp(Position.X, 15, _screenSize.X-15),
-			Math.Clamp(Position.Y, 15, _screenSize.Y-15)
+			Math.Clamp(Position.X, 15, _screenSize.X - 15),
+			Math.Clamp(Position.Y, 15, _screenSize.Y - 15)
 		);
 
 		if (_velocity.X < 0)
+		{
 			_sprite.Animation = "left";
+			_defaultCollisionPoly.Disabled = true;
+			_leftCollisionPoly.Disabled = false;
+			_rightCollisionPoly.Disabled = true;
+		}
 		else if (_velocity.X > 0)
+		{
 			_sprite.Animation = "right";
+			_defaultCollisionPoly.Disabled = true;
+			_leftCollisionPoly.Disabled = true;
+			_rightCollisionPoly.Disabled = false;
+		}
 		else
+		{
 			_sprite.Animation = "default";
+			_defaultCollisionPoly.Disabled = false;
+			_leftCollisionPoly.Disabled = true;
+			_rightCollisionPoly.Disabled = true;
+		}
 
 
 	}
+
+	private void OnAreaEntered(Area2D area)
+	{
+		if (area.IsInGroup("asteroids"))
+		{
+			GD.Print("Bang");
+		}
+
+	}
+
+
+	
 	
 }
